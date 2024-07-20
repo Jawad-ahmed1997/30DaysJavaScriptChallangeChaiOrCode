ec42236e7c449fc94f3a06672a666ca11276071c
jawad-ahmed1997 <jawad.walbi@gmail.com>
2024-07-20 16:29:43 +0500

    first commit

---
+++ day1_variableAnddatatypes.js
@@ -0,0 +1,56 @@
+// Activity 1: Variable Declaration
+
+// Task 1 : Declare a variable using var, assign it a number, and log the value to the console.
+
+var numberVariable = 10
+console.log("task 1",numberVariable)
+
+
+// Declare a variable using let, assign it a string, and log the value to the console.
+
+let stringVariable = "hello Challange chai or code"
+console.log("task 2",stringVariable)
+
+// Activity 2: Constant Declaration
+// • Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+const booleanValue = true
+console.log("task 3 ", booleanValue)
+
+// Activity 3: Data Types
+// ⚫ Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator. Activity 4: Reassigning Variables
+
+let number = 10
+let string = "hello"
+let boolean = false
+let object = {name : "jawad",age:33,skills:["html","css","javascript"]}
+let array = ["HTML","CSS","JavaScript"]
+
+console.log("number variable type is :", typeof number)
+console.log("string variable type is :", typeof string)
+console.log("boolean variable type is :", typeof boolean)
+console.log("object variable type is :", typeof object)
+console.log("array variable type is :", typeof array)
+
+// ⚫ Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+
+let letFirst = 'hello world'
+console.log('LET BEFORE reassign',letFirst)
+
+letFirst = "chai or code"
+console.log('let after reassign',letFirst)
+
+// Activity 5: Understanding const
+// ⚫ Task 6: Try reassigning a variable declared with const and observe the error.
+
+const observeError = "Declear const variable"
+// const observeError = ""
+
+// Cannot redeclare block-scoped variable 'observeError'.ts(2451)
+// const observeError = ""
+// SyntaxError: Identifier 'observeError' has already been declared
+
+
+
+
+
